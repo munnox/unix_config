5b4d8bf32bae26c283f64f569c085cd09d0a9efe
Robert Munnoch <munnox@hotmail.com>
2019-05-23 20:22:16 +0100

    Added a ansible installer script for ubuntu 18.04

---
+++ setup/install_ansible_ubuntu_1804.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Source <https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html?extIdCarryOver=true&sc_cid=701f2000001OH7YAAW#latest-releases-via-apt-ubuntu>
+# Install Ansible on ubuntu 18.04
+
+function install_ansible() {
+    sudo apt update
+    sudo apt install software-properties-common
+    sudo apt-add-repository --yes --update ppa:ansible/ansible
+    sudo apt install ansible
+}
