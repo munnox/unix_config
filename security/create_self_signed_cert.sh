92955dd669121249ca0b090657a776b009b691a3
Robert Munnoch <munnox@hotmail.com>
2020-01-02 17:16:09 +0000

    Draft Certificate building while playing with certs

---
+++ security/create_self_signed_cert.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# From https://stackoverflow.com/questions/10175812/how-to-create-a-self-signed-certificate-with-openssl
+
+# Create a Simple self signed certifcate valid for a year
+# openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
+
+cat << EOF > example-com.conf
+[ req ]
+default_bits       = 2048
+distinguished_name = req_distinguished_name
+req_extensions     = req_ext
+[ req_distinguished_name ]
+countryName                 = Country Name (2 letter code)
+stateOrProvinceName         = State or Province Name (full name)
+localityName               = Locality Name (eg, city)
+organizationName           = Organization Name (eg, company)
+commonName                 = Common Name (e.g. server FQDN or YOUR name)
+[ req_ext ]
+subjectAltName = @alternate_names
+[ alternate_names ]
+
+DNS.1       = example.com
+DNS.2       = www.example.com
+DNS.3       = mail.example.com
+DNS.4       = ftp.example.com
+
+# Add these if you need them. But usually you don't want them or
+#   need them in production. You may need them for development.
+# DNS.5       = localhost
+# DNS.6       = localhost.localdomain
+# IP.1        = 127.0.0.1
+# IP.2        = ::1
+EOF
+
+# openssl req -config example-com.conf -new -x509 -sha256 -newkey rsa:2048 -nodes \
+#     -keyout example-com.key.pem -days 365 -out example-com.cert.pem
+
+
+openssl req -config example-com.conf -new -x509 -sha256 -newkey rsa:2048 \
+    -keyout example-com.key.pem -days 365 -out example-com.cert.pem
