0fe20296aeafee01adb57ce091b73e933ec59756
Robert Munnoch <munnox@hotmail.com>
2020-03-02 21:58:47 +0000

    installing docker few tweaks

--- setup/install_docker_ubuntu_1804.sh
+++ setup/install_docker_ubuntu_1804.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Source <https://docs.docker.com/install/linux/docker-ce/ubuntu/>
 # Install Docker CE on linux
+#
+# Covenience script
+# curl -fsSL https://get.docker.com -o get-docker.sh
 
 function remove_docker() {
     sudo apt-get remove docker docker-engine docker.io containerd runc
@@ -9,17 +12,18 @@ function remove_docker() {
 function install_dockerce() {
     sudo apt-get update
     sudo apt-get install \
-	        apt-transport-https \
-		    ca-certificates \
-		        curl \
-			    gnupg-agent \
-			        software-properties-common
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg2 \
+        software-properties-common
 
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
     sudo apt-key fingerprint 0EBFCD88
 
     # Should be "9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88"
+    echo "Should be \"9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88\""
 
     sudo add-apt-repository \
        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
