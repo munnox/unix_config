41b2f268fe8caf3595b89838b86533a875bb9fcc
Robert Munnoch <munnox@hotmail.com>
2020-04-04 14:36:47 +0100

    Updated the python use script

--- use_setting.py
+++ use_setting.py
@@ -8,47 +8,96 @@
 import sys
 import os
 import shutil
+import subprocess
 
 HOME = os.getenv("HOME", None)
-LOCAL_REPO="DIR"
+PWD = os.path.abspath(".")
+LOCAL_REPO=f"{PWD}"
 print(LOCAL_REPO)
 CONFIG_PATH=os.path.join(LOCAL_REPO, "configs")
-BASH_PATTERN="source {LOCAL_REPO}//bash//bashrc".format(LOCAL_REPO=LOCAL_REPO)
-VIM_PATTERN="source {LOCAL_REPO}/configs/vim/vimrc".format(LOCAL_REPO=LOCAL_REPO)
-PATH_GIT="{LOCAL_REPO}/configs/git/gitconfig".format(LOCAL_REPO=LOCAL_REPO)
-PATH_TMUX="{LOCAL_REPO}/configs/tmux/tmux.conf".format(LOCAL_REPO=LOCAL_REPO)
-PATH_I3="{LOCAL_REPO}/configs/i3/config".format(LOCAL_REPO=LOCAL_REPO)
 
+def find_in_file(filename, pattern):
+    """Look for a pattern in a given file and report"""
+    with open(filename, "r") as f:
+        if f.read().find(pattern) >= 0:
+            return True
+    return False
+
+def run_process(*args):
+    """Simple wrapper for running a process
+    Untested
+    """
+    sp = subprocess.Popen(args)
+    sp.communicate()
+
+# Test and copy BASH config
+print("BASH Config")
+BASH_PATTERN=f"source {LOCAL_REPO}/bash/bashrc"
 CONFIG_PATH_BASH=f"{HOME}/.bashrc"
+
+if os.path.exists(CONFIG_PATH_BASH):
+    print(f"'{CONFIG_PATH_BASH}' exists looking to append")
+    if find_in_file(CONFIG_PATH_BASH, BASH_PATTERN):
+        print(f"'{CONFIG_PATH_BASH}' redirect exists continuing")
+    else:
+        with open(CONFIG_PATH_BASH, "a") as fa:
+            fa.write("\n{0}".format(BASH_PATTERN))
+        print(f"'{CONFIG_PATH_BASH}' redirect added")
+        
+# Test and copy NEOVIM
+print("NEOVIM Config")
+VIM_PATTERN=f"source {LOCAL_REPO}/configs/vim/vimrc"
 CONFIG_PATH_VIM=f"{HOME}/.vimrc"
 CONFIG_PATH_NEOVIM=f"{HOME}/.config/nvim/"
 CONFIG_FILE_NEOVIM=f"{HOME}/.config/nvim/vimrc"
 
-print(BASH_PATTERN)
-print(VIM_PATTERN)
-print(PATH_GIT)
-print(PATH_TMUX)
+if os.path.exists(CONFIG_PATH_NEOVIM):
+    print(f"'{CONFIG_PATH_NEOVIM}' folder exists")
+else:
+    os.mkdir(CONFIG_PATH_NEOVIM)
 
-def find_in_file(filename, pattern):
-    with open(filename, "r") as f:
-        if f.read().find(pattern):
-            return True
-    return False
-# TODO Test and copy BASH config
+# Redirect neovim to the home directory vim config
+with open(CONFIG_FILE_NEOVIM, "w") as f:
+    f.write("source ~/.vimrc")
 
-if os.path.exists(CONFIG_PATH_BASH):
-    print("bashrc exists")
-    if find_in_file(CONFIG_PATH_BASH, BASH_PATTERN):
-        print("Bash redirect exists")
+# Test and copy vimrc
+print("VIM Config")
+# Redirect the home vim config to the repo set
+if os.path.exists(CONFIG_PATH_VIM):
+    print(f"'{CONFIG_PATH_VIM}' exists looking to append")
+    if find_in_file(CONFIG_PATH_VIM, VIM_PATTERN):
+        print(f"'{CONFIG_PATH_VIM}' redirect to repo exists continuing")
+    else:
+        with open(CONFIG_PATH_VIM, "a") as fa:
+            fa.write("\n{0}".format(VIM_PATTERN))
+        print(f"'{CONFIG_PATH_VIM}' redirect to repo added")
 
+# Test and copy Tmux config
+print("TMUX Config")
+CONFIG_FILE_TMUX=f"{HOME}/.tmux.conf"
+PATH_TMUX=f"{LOCAL_REPO}/configs/tmux/tmux.conf"
+shutil.copy(PATH_TMUX, CONFIG_FILE_TMUX)
 
-# TODO Test and copy neovim
+# Test and copy i3 config
+print("I3 Config")
+CONFIG_FILE_I3=f"{HOME}/.config/i3/config"
+PATH_I3=f"{LOCAL_REPO}/configs/i3/config"
+try:
+    shutil.copy(PATH_I3, CONFIG_FILE_I3)
+except:
+    print("I3 potentisally not installed")
 
-# TODO Test and cope vimrc
-
-# TODO Test and copy Tmux config
-
-# TODO Test and copy i3 config
-
-
-
+# Test and copy git config
+print("GIT Config")
+CONFIG_FILE_GIT=f"{HOME}/.gitconfig"
+PATH_GIT=f"{LOCAL_REPO}/configs/git/gitconfig"
+PATTERN_GIT= f"\n[include]\npath = {PATH_GIT}"
+if os.path.exists(CONFIG_FILE_GIT):
+    if find_in_file(CONFIG_FILE_GIT, PATTERN_GIT):
+        print(f"'{CONFIG_FILE_GIT}' redirect exists continuing")
+    else:
+        with open(CONFIG_FILE_GIT, "a") as f:
+            f.write(PATTERN_GIT)
+else:
+    with open(CONFIG_FILE_GIT, "a") as f:
+        f.write(PATTERN_GIT)
