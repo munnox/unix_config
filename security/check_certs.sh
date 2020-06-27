d105543c05dbd5a42b6c2cef4354b3ea5203b210
Robert Munnoch <munnox@hotmail.com>
2020-06-27 18:13:22 +0100

    Added further information for certificate checking

--- security/check_certs.sh
+++ security/check_certs.sh
@@ -2,10 +2,28 @@
 
 # https://www.sslshopper.com/certificate-key-matcher.html
 
-# check private key
+## debug certs
+
+## check an ssl connection while printing all certs
+openssl s_client -connect www.paypal.com:443
+
+# check private key checksum
 openssl pkey -in privateKey.key -pubout -outform pem | sha256sum 
-# check certificate
+# check certificate checksum
 openssl x509 -in certificate.crt -pubkey -noout -outform pem | sha256sum 
-# check certificate request# check certificate
+# check certificate request# check certificate checksum
 openssl req -in CSR.csr -pubkey -noout -outform pem | sha256sum
 
+
+# Check cert
+## Check a Certificate Signing Request (CSR)
+openssl req -text -noout -verify -in CSR.csr
+
+## Check a private key
+openssl rsa -in privateKey.key -check
+
+## Check a certificate
+openssl x509 -in certificate.crt -text -noout
+
+## Check a PKCS#12 file (.pfx or .p12)
+openssl pkcs12 -info -in keyStore.p12
