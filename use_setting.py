83882d4cb80e0f43d2dbe651ba659075dd619bd2
Robert Munnoch <munnox@hotmail.com>
2020-07-17 16:48:46 +0100

    Updated the use_python script with pyline feedback and blacked it

--- use_setting.py
+++ use_setting.py
@@ -15,109 +15,123 @@ import subprocess
 
 HOME = os.getenv("HOME", None)
 PWD = os.path.abspath(".")
-LOCAL_REPO=f"{PWD}"
+LOCAL_REPO = f"{PWD}"
 print(LOCAL_REPO)
-CONFIG_PATH=os.path.join(LOCAL_REPO, "configs")
+CONFIG_PATH = os.path.join(LOCAL_REPO, "configs")
+
 
 def find_in_file(filename, pattern):
     """Look for a pattern in a given file and report"""
-    with open(filename, "r") as f:
-        if f.read().find(pattern) >= 0:
+    with open(filename, "r") as file_handle:
+        if file_handle.read().find(pattern) >= 0:
             return True
     return False
+
 
 def run_process(*args):
     """Simple wrapper for running a process
     Untested
     """
-    sp = subprocess.Popen(args)
-    sp.communicate()
+    sub_process = subprocess.Popen(args)
+    sub_process.communicate()
+
 
 def fix_bash():
+    """Install bash config file"""
     # Test and copy BASH config
     print("========== BASH Config ==========")
-    BASH_PATTERN=f"source {LOCAL_REPO}/bash/bashrc"
-    CONFIG_PATH_BASH=f"{HOME}/.bashrc"
+    bash_pattern = f"source {LOCAL_REPO}/bash/bashrc"
+    config_path_bash = f"{HOME}/.bashrc"
 
-    if os.path.exists(CONFIG_PATH_BASH):
-        print(f"'{CONFIG_PATH_BASH}' exists looking to append")
-        if find_in_file(CONFIG_PATH_BASH, BASH_PATTERN):
-            print(f"'{CONFIG_PATH_BASH}' redirect exists continuing")
+    if os.path.exists(config_path_bash):
+        print(f"'{config_path_bash}' exists looking to append")
+        if find_in_file(config_path_bash, bash_pattern):
+            print(f"'{config_path_bash}' redirect exists continuing")
         else:
-            with open(CONFIG_PATH_BASH, "a") as fa:
-                fa.write("\n{0}".format(BASH_PATTERN))
-            print(f"'{CONFIG_PATH_BASH}' redirect added")
-        
+            with open(config_path_bash, "a") as file_handle:
+                file_handle.write("\n{0}".format(bash_pattern))
+            print(f"'{config_path_bash}' redirect added")
+
+
 def fix_neovim():
+    """Install neovim config file"""
     # Test and copy NEOVIM
     print("========== NEOVIM Config ==========")
-    VIM_PATTERN=f"source {LOCAL_REPO}/configs/vim/vimrc"
-    CONFIG_FILE_VIM=f"{HOME}/.vimrc"
-    CONFIG_PATH_NEOVIM=f"{HOME}/.config/nvim/"
-    CONFIG_FILE_NEOVIM=f"{CONFIG_PATH_NEOVIM}/init.vim"
+    vim_pattern = f"source {LOCAL_REPO}/configs/vim/vimrc"
+    config_file_vim = f"{HOME}/.vimrc"
+    config_path_neovim = f"{HOME}/.config/nvim/"
+    config_file_neovim = f"{config_path_neovim}/init.vim"
 
-    if os.path.exists(CONFIG_PATH_NEOVIM):
-        print(f"'{CONFIG_PATH_NEOVIM}' folder exists")
+    if os.path.exists(config_path_neovim):
+        print(f"'{config_path_neovim}' folder exists")
+
     else:
-        os.mkdir(CONFIG_PATH_NEOVIM)
+        os.mkdir(config_path_neovim)
 
-    # Redirect neovim to the home directory vim config
-    with open(CONFIG_FILE_NEOVIM, "w") as f:
-        f.write("source ~/.vimrc")
+    # Redirect neovim to the HOME directory vim config
+    with open(config_file_neovim, "w") as file_handle:
+        file_handle.write("source ~/.vimrc")
 
-    # Test and copy vimrc
+    # config_file_vim Test and copy vimrc
     print("========== VIM Config ==========")
-    # Redirect the home vim config to the repo set
-    if os.path.exists(CONFIG_FILE_VIM):
-        print(f"'{CONFIG_FILE_VIM}' exists looking to append")
-        if find_in_file(CONFIG_FILE_VIM, VIM_PATTERN):
-            print(f"'{CONFIG_FILE_VIM}' redirect to repo exists continuing")
+    # Redirect the HOME vim config to the repo set
+    if os.path.exists(config_file_vim):
+        print(f"'{config_file_vim}' exists looking to append")
+        if find_in_file(config_file_vim, vim_pattern):
+            print(f"'{config_file_vim}' redirect to repo exists continuing")
         else:
-            with open(CONFIG_FILE_VIM, "a") as fa:
-                fa.write("\n{0}".format(VIM_PATTERN))
-            print(f"'{CONFIG_FILE_VIM}' redirect to repo added")
+            with open(config_file_vim, "a") as file_handle:
+                file_handle.write("\n{0}".format(vim_pattern))
+            print(f"'{config_file_vim}' redirect to repo added")
     else:
-        with open(CONFIG_FILE_VIM, "w") as f:
-            f.write(VIM_PATTERN)
-        print(f"'{CONFIG_FILE_VIM}' file created and redirect to repo set")
+        with open(config_file_vim, "w") as file_handle:
+            file_handle.write(vim_pattern)
+        print(f"'{config_file_vim}' file created and redirect to repo set")
+
 
 def fix_tmux():
+    """Install tmux config file"""
     # Test and copy Tmux config
     print("========== TMUX Config ==========")
-    CONFIG_FILE_TMUX=f"{HOME}/.tmux.conf"
-    PATH_TMUX=f"{LOCAL_REPO}/configs/tmux/tmux.conf"
+    config_file_tmux = f"{HOME}/.tmux.conf"
+    path_tmux = f"{LOCAL_REPO}/configs/tmux/tmux.conf"
     try:
-        shutil.copy(PATH_TMUX, CONFIG_FILE_TMUX)
-        print(f"Copying Tmux config as cannot link to repo to:\n{PATH_TMUX}")
-    except Exception as error:
-        print("Tmux config.\nError:\n", error)
+        shutil.copy(path_tmux, config_file_tmux)
+        print(f"Copying Tmux config as cannot link to repo to:\n{path_tmux}")
+    except Exception as error:  # pylint: disable=broad-except
+        print(f"Tmux config.\nError type: ({type(error)}):\n{error}")
+
 
 def fix_i3():
+    """Install i3 window env config file"""
     # Test and copy i3 config
     print("========== I3 Config ==========")
-    CONFIG_FILE_I3=f"{HOME}/.config/i3/config"
-    PATH_I3=f"{LOCAL_REPO}/configs/i3/config"
+    config_file_i3 = f"{HOME}/.config/i3/config"
+    path_i3 = f"{LOCAL_REPO}/configs/i3/config"
     try:
-        shutil.copy(PATH_I3, CONFIG_FILE_I3)
-        print(f"Copying I3 config as cannot link to repo to:\n{PATH_I3}")
-    except Exception as error:
-        print("I3 potentially not installed.\nError:\n", error)
+        shutil.copy(path_i3, config_file_i3)
+        print(f"Copying I3 config as cannot link to repo to:\n{path_i3}")
+    except Exception as error:  # pylint: disable=broad-except
+        print(f"I3 potentially not installed.\nError type: ({type(error)}):\n{error}")
+
 
 def fix_git():
+    """Install git config file"""
     # Test and copy git config
     print("========== GIT Config ==========")
-    CONFIG_FILE_GIT=f"{HOME}/.gitconfig"
-    PATH_GIT=f"{LOCAL_REPO}/configs/git/gitconfig"
-    PATTERN_GIT= f"\n[include]\npath = {PATH_GIT}"
-    if os.path.exists(CONFIG_FILE_GIT):
-        if find_in_file(CONFIG_FILE_GIT, PATTERN_GIT):
-            print(f"'{CONFIG_FILE_GIT}' redirect exists continuing")
+    config_file_git = f"{HOME}/.gitconfig"
+    path_git = f"{LOCAL_REPO}/configs/git/gitconfig"
+    pattern_git = f"\n[include]\npath = {path_git}"
+    if os.path.exists(config_file_git):
+        if find_in_file(config_file_git, pattern_git):
+            print(f"'{config_file_git}' redirect exists continuing")
         else:
-            with open(CONFIG_FILE_GIT, "a") as f:
-                f.write(PATTERN_GIT)
+            with open(config_file_git, "a") as file_handle:
+                file_handle.write(pattern_git)
     else:
-        with open(CONFIG_FILE_GIT, "a") as f:
-            f.write(PATTERN_GIT)
+        with open(config_file_git, "a") as file_handle:
+            file_handle.write(pattern_git)
+
 
 if __name__ == "__main__":
 
