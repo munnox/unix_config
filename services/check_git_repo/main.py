b8f19283e8ff29e0e7f969edf60786b892ab2c5d
Robert Munnoch <munnox@hotmail.com>
2020-11-01 00:36:49 +0000

    Added a open url and a better path resolver

--- services/check_git_repo/main.py
+++ services/check_git_repo/main.py
@@ -3,6 +3,7 @@ import click
 
 #  https://gitpython.readthedocs.io/en/stable/tutorial.html
 from git import Repo
+from git.exc import GitCommandError
 
 class bcolors:
     """console colors
@@ -19,24 +20,33 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def process_repo(root, url, push):
+def process_repo(root, url, push, pull):
     repo = Repo(root)
     assert not repo.bare
 
     remotes_matched = []
     for remote in repo.remotes:
-        if remote.url.find(url) >= 0:
-            #  for repo_branch in repo.heads:
+        if url != "":
+            if remote.url.find(url) >= 0:
+                #  for repo_branch in repo.heads:
+                remotes_matched.append(remote)
+        else:
             remotes_matched.append(remote)
+
     logs = []
     if len(remotes_matched) > 0:
         logs.append(f"{bcolors.HEADER}remote: {root}{bcolors.ENDC}")
+        print(f"{bcolors.HEADER}remote: {root}{bcolors.ENDC}")
         logs.append(f"Repo Branches: {[b.name for b in repo.heads]}")
         for remote in remotes_matched:
             logs.append(f"Remotes: {remote.name} = {remote.url}")
             logs.append(f"Remote Refs: {[b.name for b in remote.refs]}")
-            fetch_results = remote.fetch()
-            logs.append(f"fetching remotes")
+            try:
+                fetch_results = remote.fetch()
+                logs.append(f"fetching remotes")
+            except GitCommandError as error:
+                logs.append(f"{bcolors.FAIL} unable to fetch remote {remote} {error}")
+
             for repo_branch in repo.heads:
                 #  print(f"repo_branch: {repo_branch}")
                 for remote_refs in remote.refs:
@@ -50,24 +60,33 @@ def process_repo(root, url, push):
                             if push:
                                 pushinfolist = remote.push() 
                                 for pushinfo in pushinfolist:
-                                    logs.append(f"Pushing to remote {pushinfo.summary} was at {pushinfo.old_commit}")
+                                    logs.append(f"{bcolors.OKGREEN}Pushing to remote {pushinfo.summary} was at {pushinfo.old_commit}{bcolors.ENDC}")
+                        if len(behind_commits) > 0 and len(ahead_commits) == 0:
+                            logs.append(f"{bcolors.WARNING}Would be useful to pull{bcolors.ENDC}")
+                            if pull:
+                                pullinfolist = remote.pull() 
+                                for pullinfo in pullinfolist:
+                                    logs.append(f"{bcolors.OKGREEN}Pulling to remote {pullinfo.summary} was at {pullinfo.old_commit}{bcolors.ENDC}")
             logs.append("")
     return logs
 
 @click.command()
 #  @click.option('--count', default=1, help='Number of greetings.')
-@click.option("--path", prompt="Enter path", help="Enter the path to the repo dir")
-@click.option("--url", prompt="Enter url to push", help="Enter the url to the push")
+@click.option("--path", prompt="Enter path", help="Enter the path to the repo dir", default=".")
+@click.option("--url", prompt="Enter url to push", help="Enter the url to the push", default="")
 @click.option('--push', is_flag=True, default=False)
-def main(path, url, push):
+@click.option('--pull', is_flag=True, default=False)
+def main(path, url, push, pull):
     """Simple program to find git repos within a given path."""
     logs = []
-    for root, dirs, files in os.walk(path, topdown=False):
+    full_path = os.path.abspath(os.path.expanduser(path))
+    print(f"Running git repo checker in {full_path}")
+    for root, dirs, files in os.walk(full_path, topdown=False):
         #  for name in files:
         #      print(os.path.join(root, name))
         if ".git" in dirs:
             #  print(f"git directory found: {root}")
-            logs += process_repo(root, url, push)
+            logs += process_repo(root, url, push, pull)
         #  for name in dirs:
         #      print(os.path.join(root, name))
     log_str = "\n".join(logs)
