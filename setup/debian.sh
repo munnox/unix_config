a39b1ea2dcb0115c5d39214b06def9cf3419a3b1
Robert Munnoch <munnox@hotmail.com>
2020-11-13 07:57:19 +0000

    Added a virtual tester

--- setup/debian.sh
+++ setup/debian.sh
@@ -116,3 +116,13 @@ function install_kvm() {
 
 }
 
+# ======================== VirtualBox =========================
+# source https://www.virtualbox.org/wiki/Linux_Downloads
+
+function install_virtualbox() {
+    wget https://download.virtualbox.org/virtualbox/6.1.16/virtualbox-6.1_6.1.16-140961~Ubuntu~eoan_amd64.deb
+
+    sudo apt install ./virtualbox-6.1_6..16-140961-Ubuntu-eon_and64.deb
+
+}
+
