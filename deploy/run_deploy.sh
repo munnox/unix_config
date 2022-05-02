cea5a99506896d18a0d1a4a1761478b6f58b658d
Robert Munnoch <robert@unix_config.git>
2022-05-02 21:51:35 +0000

    Moved deploymen to poetry from Pipenv

--- deploy/run_deploy.sh
+++ deploy/run_deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-pipenv run ansible-playbook deploy.yml -K
+sudo dnf install -y python3.9 python3-pip
+sudo python3.9 -m pip install poetry
+poetry install
+poetry run ansible-playbook deploy.yml -K
