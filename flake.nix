18327c46677cd86d22b7297a911a79d3764dd087
Robert Munnoch <robert@munnox.com>
2022-10-30 20:11:22 +0000

    Update flake for deploy

--- flake.nix
+++ flake.nix
@@ -7,14 +7,10 @@
       (system:
         let pkgs = nixpkgs.legacyPackages.${system}; in
         {
-          # devShells."${system}".default = import ./shell.nix { inherit pkgs; };
-          # devShells."aarch64-linux".default = import ./shell.nix { inherit pkgs; };
-          # devShells."x86_64-darwin".default = import ./shell.nix { inherit pkgs; };
           packages.hello = pkgs.hello;
-          packages.shell = import ./deploy/shell.nix { inherit pkgs; };
-          packages.default = self.packages.${system}.shell;
-          # devShells.aarch64-darwin.default = import ./shell.nix { inherit pkgs; };
-          devShells.default = self.packages.${system}.shell;
+          packages.deployshell = import ./deploy/shell.nix { inherit pkgs; };
+          packages.default = self.packages.${system}.deployshell;
+          devShells.default = self.packages.${system}.deployshell;
         }
       );
   # outputs = { self, nixpkgs }: {
