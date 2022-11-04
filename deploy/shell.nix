909bf98e014899e130135260d07d84f4673b5265
Robert Munnoch <robert@munnox.com>
2022-11-04 15:54:02 +0000

    Better QoL experience on the nix shell
    Added a name correction and some other useful packages as options

--- deploy/shell.nix
+++ deploy/shell.nix
@@ -75,6 +75,10 @@ let
     echo "Running playbook: $1 on $2"
     ${ansible_playbook} ${all_inventories} --extra-vars "playbook_groups=$1" $2
   '';
+  runplayhostpass = pkgs.writeScriptBin "runplayhostpass" ''
+    echo "Running playbook with become password: $1 on $2"
+    ${ansible_playbook} ${all_inventories} --extra-vars "playbook_groups=$1" $2 -K
+  '';
   basepkgs = [
     pkgs.poetry
     pkgs.ansible
@@ -93,16 +97,68 @@ let
     runplayonly
     runplay
     runplayhost
+    runplayhostpass
   ];
+  # config = ./config/bash;
+  # configpath = builtins.toString ./.;
 in pkgs.mkShell {       # mkShell is a helper function
   name="deploy_ctl";    # that requires a name
-  buildInputs = if pkgs.stdenv.isDarwin then basepkgs ++ scripthelpers else basepkgs ++ [
-      pkgs.azure-cli
-      pkgs.aws
-    ] ++ scripthelpers;
-
+  # And a list of build inputs
+  buildInputs = if pkgs.stdenv.isDarwin then 
+      basepkgs ++ scripthelpers
+    else 
+        basepkgs ++ [
+        pkgs.azure-cli
+        pkgs.awscli
+      ] ++ scripthelpers;
+  # Then will run this script before give the user the shell
   shellHook = ''
+    # declare -xp
+    #set -xp
+    # Colours
+    txtblk='\[\e[0;30m\]' # Black - Regular
+    txtred='\[\e[0;31m\]' # Red
+    txtgrn='\[\e[0;32m\]' # Green
+    txtylw='\[\e[0;33m\]' # Yellow
+    txtblu='\[\e[0;34m\]' # Blue
+    txtpur='\[\e[0;35m\]' # Purple
+    txtcyn='\[\e[0;36m\]' # Cyan
+    txtwht='\[\e[0;37m\]' # White
+
+    bldblk='\[\e[1;30m\]' # Black - Bold
+    bldred='\[\e[1;31m\]' # Red
+    bldgrn='\[\e[1;32m\]' # Green
+    bldylw='\[\e[1;33m\]' # Yellow
+    bldblu='\[\e[1;34m\]' # Blue
+    bldpur='\[\e[1;35m\]' # Purple
+    bldcyn='\[\e[1;36m\]' # Cyan
+    bldwht='\[\e[1;37m\]' # White
+
+    unkblk='\[\e[4;30m\]' # Black - Underline
+    undred='\[\e[4;31m\]' # Red
+    undgrn='\[\e[4;32m\]' # Green
+    undylw='\[\e[4;33m\]' # Yellow
+    undblu='\[\e[4;34m\]' # Blue
+    undpur='\[\e[4;35m\]' # Purple
+    undcyn='\[\e[4;36m\]' # Cyan
+    undwht='\[\e[4;37m\]' # White
+
+    bakblk='\[\e[40m\]'   # Black - Background
+    bakred='\[\e[41m\]'   # Red
+    badgrn='\[\e[42m\]'   # Green
+    bakylw='\[\e[43m\]'   # Yellow
+    bakblu='\[\e[44m\]'   # Blue
+    bakpur='\[\e[45m\]'   # Purple
+    bakcyn='\[\e[46m\]'   # Cyan
+    bakwht='\[\e[47m\]'   # White
+    txtrst='\[\e[0m\]'    # Text Reset
+
+    alias ll='ls -al'
+
     # bash to run when you enter the shell
+    PROMPT=$bldgrn"NIX-\u@\h ["$bldpur"\w"$bldgrn"]"$txtrst'$(BRANCH=`git rev-parse --abbrev-ref HEAD 2> /dev/null`; if [ -n "$BRANCH" ]; then DIRTY=`git status --porcelain 2> /dev/null`; if [ -n "$DIRTY" ]; then echo "'$txtylw' ($BRANCH) '$txtrst'"; else echo "'$txtgrn' ($BRANCH) '$txtrst'"; fi fi;)'$txtcyn"\n\$ "$txtrst
+    OLD_PS1=$PS1
+    export PS1=$PROMPT
     echo "Start developing...system = '${pkgs.system}'"                                        
     # poetry install
     # poetry shell
