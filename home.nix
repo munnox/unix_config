6931f531c7bcb649624a64c41bc4642cbf9e9aec
Robert Munnoch <robert@munnox.com>
2022-11-20 00:27:45 +0000

    Added gitconfig to nix home

--- home.nix
+++ home.nix
@@ -32,6 +32,132 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   
+  programs.git.enable = true;
+  programs.git = {
+    # enable = true;
+    userName = "ghedamat";
+    userEmail = "ghedamat@gmail.com";
+    aliases = {
+      remotes = "remote -v";
+      branches = "branch -a";
+      tags = "tag -l";
+      
+      st = "status";
+      sst  = "status -sb";
+      co   = "checkout";
+      currentbranch = "rev-parse --abbrev-ref HEAD";
+      l = "log --decorate --oneline --graph --color";
+      la = "log --decorate --oneline --graph --color --all";
+      fa = "fetch -p --all";
+      fo = "fetch -p origin";
+      f  = "fetch -p";
+      b  = "branch -a";
+      bd = "branch -d";
+
+      sl = "stash list";
+      sa = "stash apply";
+      ss = "stash save";
+
+      r     = "reset";
+      r1    = "reset HEAD^";
+      r2    = "reset HEAD^^";
+      rh    = "reset --hard";
+      rh1   = "reset HEAD^ --hard";
+      rh2   = "reset HEAD^^ --hard";
+      wdiff = "diff --color-words";
+      addcw = "-c interactive.diffFilter='git diff --color-words'";
+      fullhistory    = "log --all --decorate --oneline --graph";
+      # customlogrel   = ''log --pretty=format:"%C(auto,yellow)%h%C(auto,magenta)% G? %C(auto,blue)%>(12,trunc)%ad %C(auto,green)%<(7,trunc)%aN%C(auto,reset)%s%C(auto,red)% gD% D" --date=relative' '';
+      customlogshort = ''log --pretty=format:'%C(auto,yellow)%h%C(auto,magenta)% G? %C(auto,blue)%>(12,trunc)%ad %C(auto,green)%<(7,trunc)%aN%C(auto,reset)%s%C(auto,red)% gD% (%D)' '';
+      longlog     = ''log --format='%C(auto,yellow)%h%C(auto,magenta)% G? %C(auto,blue)%>(12,trunc)%ad %C(auto,green)%<(10,trunc)%aN%C(auto,reset)%s%C(auto,red)% gD% D' '';
+      longlonglog = ''log --date=relative --format='%C(auto,yellow)%h%C(auto,magenta)% G? %C(auto,blue)%ad | %cd %C(auto,green)%<(10,trunc)%aN%C(auto,reset)%s%C(auto,red)% gD% D' '';
+      logrecord   = ''log --date=relative --format='%C(auto,yellow)%h %C(auto,magenta)Authored: %ad %C(auto,blue) Commited: %cd %C(auto,green)%aN (%aE)%C(auto,reset) -> %s%C(auto,red)% gD% D' --date=iso --graph"'';
+    };
+  };
+  # programs.git.config = {
+  #   init = {
+  #     defaultBranch = "main";
+  #   };
+  #   url = {
+  #     "https:/github.com/" = {
+  #       insteadOf = [
+  #         "gh:"
+  #         "github:"
+  #       ];
+  #     };
+  #     "https:/gitlab.com/" = {
+  #       insteadOf = [
+  #         "gl:"
+  #         "gitlab:"
+  #       ];
+  #     };
+  #   };
+  #   core = {
+  #     editor = "hx";
+  #   };
+  #   user = {
+  #     name = "Robert Munnoch";
+  #     email = "robert@unix_config.git";
+  #   };
+  #   push.default = "simple";
+  #   pull.ff = "only";
+  #   # include.path = [ 
+  #   #   "${./configs/git}git_diff.ini"
+  #   #   "git_alias.ini"
+  #   # ];
+  #   alias = {
+  #     remotes = "remote -v";
+  #     branches = "branch -a";
+  #     tags = "tag -l";
+      
+  #     currentbranch = "rev-parse --abbrev-ref HEAD";
+  #     l = "log --decorate --oneline --graph --color";
+  #     la = "log --decorate --oneline --graph --color --all";
+      
+  #     id = "rev-parse --short HEAD";
+      
+  #     lg   = "log --color --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'";
+  #     ls   = "log --pretty=format:'%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]' --decorate";
+  #     ll   = "log --pretty=format:'%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]' --decorate --numstat";
+  #     diff = "diff --word-diff";
+  #     dc   = "diff --cached";
+  #     st   = "status # -sb";
+  #     sst  = "status -sb";
+  #     co   = "checkout";
+
+  #     fa = "fetch -p --all";
+  #     fo = "fetch -p origin";
+  #     f  = "fetch -p";
+  #     b  = "branch -a";
+  #     bd = "branch -d";
+
+  #     sl = "stash list";
+  #     sa = "stash apply";
+  #     ss = "stash save";
+
+  #     r     = "reset";
+  #     r1    = "reset HEAD^";
+  #     r2    = "reset HEAD^^";
+  #     rh    = "reset --hard";
+  #     rh1   = "reset HEAD^ --hard";
+  #     rh2   = "reset HEAD^^ --hard";
+  #     wdiff = "diff --color-words";
+  #     addcw = "-c interactive.diffFilter='git diff --color-words'";
+  #     fullhistory    = "log --all --decorate --oneline --graph";
+  #     # customlogrel   = ''log --pretty=format:"%C(auto,yellow)%h%C(auto,magenta)% G? %C(auto,blue)%>(12,trunc)%ad %C(auto,green)%<(7,trunc)%aN%C(auto,reset)%s%C(auto,red)% gD% D" --date=relative' '';
+  #     customlogshort = ''log --pretty=format:'%C(auto,yellow)%h%C(auto,magenta)% G? %C(auto,blue)%>(12,trunc)%ad %C(auto,green)%<(7,trunc)%aN%C(auto,reset)%s%C(auto,red)% gD% (%D)' '';
+  #     longlog     = ''log --format='%C(auto,yellow)%h%C(auto,magenta)% G? %C(auto,blue)%>(12,trunc)%ad %C(auto,green)%<(10,trunc)%aN%C(auto,reset)%s%C(auto,red)% gD% D' '';
+  #     longlonglog = ''log --date=relative --format='%C(auto,yellow)%h%C(auto,magenta)% G? %C(auto,blue)%ad | %cd %C(auto,green)%<(10,trunc)%aN%C(auto,reset)%s%C(auto,red)% gD% D' '';
+  #     logrecord   = ''log --date=relative --format='%C(auto,yellow)%h %C(auto,magenta)Authored: %ad %C(auto,blue) Commited: %cd %C(auto,green)%aN (%aE)%C(auto,reset) -> %s%C(auto,red)% gD% D' --date=iso --graph"'';
+  #     graphviz    = ''!"f() { echo 'digraph git {' ; git log --pretty='format: %h -> { %p }' \"$@\" | sed 's/[0-9a-f][0-9a-f]*/\"&\"/g' ; echo '}'; }; f"'';
+  #     # cmd got alias above `git graphviz | dot -Tpng -o`
+
+  #     sync = ''!sh -c 'git checkout --quiet --detach HEAD && \
+  #                    git fetch origin master:master ; \
+  #                    git checkout --quiet -' '';
+  #   };
+  # };
+  
   programs.fish.enable = true;
   programs.fish.interactiveShellInit = ''
 
